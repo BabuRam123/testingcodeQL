9e69397edce6dc645fdda760b0341b85fa925da6
BabuRam123 <55832224+BabuRam123@users.noreply.github.com>
2022-08-03 17:30:30 +0530

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace WindowsFormsApplication1
 {
@@ -22,6 +23,21 @@ namespace WindowsFormsApplication1
             MessageBox.Show("hi");
             string password = "hi";
             MessageBox.Show(password);
+            String str = "server=MUNESH-PC;database=windowapp;UID=sa;password=123";
+
+            String query = "select * from data";
+
+            SqlConnection con = new SqlConnection(str);
+
+            SqlCommand cmd = new SqlCommand(query, con);
+
+            con.Open();
+
+            DataSet ds = new DataSet();
+
+            MessageBox.Show("connect with sql server");
+
+            con.Close();
         }
     }
 }
